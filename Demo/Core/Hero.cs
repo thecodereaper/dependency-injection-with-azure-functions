7b4153329ed345f6e2e3cfb2bcee2fcdeca6ba27
theCodeReaper <hello@theCodeReaper.com>
2020-05-23 21:20:13 +0200

    Initial commit.
    Initial commit.

---
+++ Demo/Core/Hero.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Demo.Core
+{
+    public class Hero
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Demo/Core/IHeroRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Demo.Core
+{
+    public interface IHeroRepository
+    {
+        Task<IEnumerable<Hero>> GetAllAsync();
+    }
+}
