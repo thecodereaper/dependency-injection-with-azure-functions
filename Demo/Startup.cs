5300ebdfb367a30eed22e238b802fde5ea690228
theCodeReaper <hello@theCodeReaper.com>
2020-05-23 23:35:33 +0200

    Initial commit.

---
+++ Demo/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Demo.Core;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Demo.Startup))]
+
+namespace Demo
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<IHeroRepository, HeroRepository>();
+        }
+    }
+}
